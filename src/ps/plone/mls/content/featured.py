6d49a589ac1f4002ae204ee879f7aa49d1b7c8e3
Thomas Massmann <thomas@propertyshelf.com>
2014-07-06 21:35:24 +0200

    Added 'Featured Listings' content type.

---
+++ src/ps/plone/mls/content/featured.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+# zope imports
+from Products.ATContentTypes import ATCTMessageFactory as ATMF
+from plone.app.textfield import RichText
+from plone.app.textfield.widget import RichTextFieldWidget
+from plone.autoform import directives
+from plone.supermodel import model
+from z3c.form.browser.multi import multiFieldWidgetFactory
+from zope import schema
+
+# local import
+from ps.plone.mls import _
+
+
+class IFeaturedListings(model.Schema):
+    """A list of featured MLS Listings."""
+
+    directives.widget(listing_ids=multiFieldWidgetFactory)
+    listing_ids = schema.List(
+        description=_(u'Add one Listing ID for each entry to show up.'),
+        title=_(u'MLS Listing IDs'),
+        unique=True,
+        value_type=schema.TextLine(
+            title=_(u'ID'),
+        ),
+    )
+
+    directives.widget(body_text=RichTextFieldWidget)
+    body_text = RichText(
+        required=False,
+        title=ATMF(u'label_body_text', default=u'Body Text'),
+    )
