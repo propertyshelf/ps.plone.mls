fe7babf7d1862b27412dec3590f635bf76db0a3f
Thomas Massmann <thomas@propertyshelf.com>
2015-02-02 14:47:43 +0100

    Show a google map with the development pinned.

--- src/ps/plone/mls/browser/developments/details.py
+++ src/ps/plone/mls/browser/developments/details.py
@@ -14,6 +14,43 @@ from zope.interface import implementer
 from ps.plone.mls import api
 from ps.plone.mls.interfaces import IDevelopmentDetails
 
+MAP_JS = """
+function initializeMap() {{
+    var center = new google.maps.LatLng({lat}, {lng})
+    var myOptions = {{
+        zoom: {zoom},
+        center: center,
+        mapTypeId: google.maps.MapTypeId.TERRAIN,
+        mapTypeControl: true,
+        disableDoubleClickZoom: true,
+        streetViewControl: true
+    }}
+
+    var map = new google.maps.Map(
+        document.getElementById('{map_id}'),
+        myOptions
+    );
+
+    var has_marker = true;
+    if(has_marker) {{
+        var myLatlng = new google.maps.LatLng({lat}, {lng});
+        var marker = new google.maps.Marker({{
+            position: myLatlng,
+            map: map
+        }});
+    }}
+    return map;
+}};
+
+map = initializeMap();
+google.maps.event.addDomListener(window, "resize", function() {{
+    var center = map.getCenter();
+    google.maps.event.trigger(map, "resize");
+    map.setCenter(center);
+}});
+
+"""
+
 
 @implementer(IDevelopmentDetails)
 class DevelopmentDetails(BrowserView):
@@ -45,3 +82,24 @@ class DevelopmentDetails(BrowserView):
             except ConnectionError:
                 pass
         self.item = item
+
+    @property
+    def map_id(self):
+        """Generate a unique css id for the map."""
+        try:
+            item_id = self.item.id.value
+        except:
+            item_id = 'unknown'
+        return u'map__{0}'.format(item_id)
+
+    def javascript_map(self):
+        """Return the JS code for the map."""
+        if not hasattr(self.item, 'geolocation'):
+            return
+        lat, lng = self.item.geolocation.value.split(',')
+        return MAP_JS.format(
+            lat=lat,
+            lng=lng,
+            map_id=self.map_id,
+            zoom=7,
+        )
