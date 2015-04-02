f78f3ea39fc4abeb0804e9f1f32536605020a098
Jens Krause <jens@propertyshelf.com>
2015-04-03 00:16:01 +0700

    add function to reset amenity icon height as needed

--- src/ps/plone/mls/browser/static/mls.js
+++ src/ps/plone/mls/browser/static/mls.js
@@ -1,3 +1,10 @@
+function resize_icons(icons){
+  // adjust height & width to make the icons fit in one line
+  icon_width = jQuery(icons[0]).width();
+  // square the icon
+  jQuery(icons).height(icon_width);
+}
+
 jQuery(function(jq) {
 
   if (jq('.mls .development__gallery .thumbnails').length > 0) {
@@ -43,5 +50,15 @@ jQuery(function(jq) {
       closeselector: '[name="form.buttons.cancel"]'
     });
   }
+  icons = jq('.amenitiesicon');
+  if(icons.length>0){
+    resize_icons(icons);
+  }
 
+});
+jQuery(window).resize(function() {
+  icons = jQuery('.development__amenities .amenitiesicon');
+  if(icons.length>0){
+    resize_icons(icons);
+  }
 });
