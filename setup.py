259c428209678460bf83e9880e1ceb57bc55d4df
Jens Krause <jens@propertyshelf.com>
2015-08-03 16:27:48 +0700

    count up dev nr.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 
 from setuptools import setup, find_packages
 
-version = '0.2.6.dev4'
+version = '0.2.6.dev5'
 description = "Propertyshelf MLS Plone Embedding."
 long_description = ('\n'.join([
     open('README.rst').read(),
