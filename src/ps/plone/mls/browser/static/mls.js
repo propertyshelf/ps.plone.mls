d8ee3ae700bcca51d737355b517aadc4fab590b3
Thomas Massmann <thomas@propertyshelf.com>
2017-04-19 15:30:58 +0200

    No modal for MLS configuration views.

--- src/ps/plone/mls/browser/static/mls.js
+++ src/ps/plone/mls/browser/static/mls.js
@@ -29,26 +29,4 @@ jQuery(function(jq) {
     });
   }
 
-  if (jq('#content-views #contentview-featured-listings-config').length > 0) {
-    // Show the featured listing configuration form with a nice overlay.
-    jq('#content-views #contentview-featured-listings-config > a').prepOverlay({
-      subtype: 'ajax',
-      filter: '#content>*',
-      formselector: '#content-core > form',
-      noform: 'reload',
-      closeselector: '[name="form.buttons.cancel"]'
-    });
-  }
-
-  if (jq('#content-views #contentview-development-collection-config').length > 0) {
-    // Show the development collection configuration form with a nice overlay.
-    jq('#content-views #contentview-development-collection-config > a').prepOverlay({
-      subtype: 'ajax',
-      filter: '#content>*',
-      formselector: '#content-core > form',
-      noform: 'reload',
-      closeselector: '[name="form.buttons.cancel"]'
-    });
-  }
-
 });
