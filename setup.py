40cb731df1e9925a3d4d5a87e960cbe5c254b87d
Thomas Massmann <thomas@propertyshelf.com>
2017-05-12 13:22:30 +0200

    Back to development: 0.9

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 
 from setuptools import setup, find_packages
 
-version = '0.8'
+version = '0.9.dev0'
 description = "Propertyshelf MLS Plone Embedding."
 long_description = ('\n'.join([
     open('README.rst').read(),
