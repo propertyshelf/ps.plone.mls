2fdcf45ffe82b46c5264cd052b0619b4ee383ef0
Thomas Massmann <thomas@propertyshelf.com>
2017-05-25 19:57:14 -0400

    Use plone.api.content.find method and sort by title.

--- src/ps/plone/mls/vocabularies/listings.py
+++ src/ps/plone/mls/vocabularies/listings.py
@@ -3,6 +3,7 @@
 
 # zope imports
 from plone import api
+from plone.mls.listing.browser.listing_search import IListingSearch
 from zope.interface import implementer
 from zope.schema.interfaces import IVocabularyFactory
 from zope.schema.vocabulary import SimpleTerm, SimpleVocabulary
@@ -22,11 +23,10 @@ class AvailableListingSearches(object):
     """Vocabulary which returns all available listing searches."""
 
     def __call__(self, context):
-        catalog = api.portal.get_tool(name='portal_catalog')
-        brains = catalog.search({
-            'object_provides': 'plone.mls.listing.browser.listing_search.'
-                               'IListingSearch',
-        })
+        brains = api.content.find(
+            object_provides=IListingSearch,
+            sort_on='sortable_title',
+        )
         items = []
         for brain in brains:
             title = '{0} ({1})'.format(brain.Title, brain.getPath())
