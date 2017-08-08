dc16b109b753943fb28e681b23ca627d8fcdc340
Thomas Massmann <thomas@propertyshelf.com>
2017-08-08 12:41:39 +0200

    Add import step to install additional add-ons, if available.
    Not using post_install handler to not depend on newer GS versions.

--- src/ps/plone/mls/setuphandlers.py
+++ src/ps/plone/mls/setuphandlers.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 """Post install import steps for ps.plone.mls."""
 
+# python imports
+import pkg_resources
+
 # zope imports
 from Products.CMFPlone.interfaces import INonInstallable
 from Products.GenericSetup.interfaces import IProfileImportedEvent
@@ -10,6 +13,11 @@ from zope.interface import implementer
 
 # local imports
 from ps.plone.mls import config
+
+
+ADD_ONS = [
+    'ps.plone.fotorama',
+]
 
 
 @implementer(INonInstallable)
@@ -30,3 +38,25 @@ def handle_profile_imported_event(event):
     if event.profile_id == 'profile-plone.app.upgrade.v50:to50alpha3':
         setup = api.portal.get_tool(name='portal_setup')
         setup.runAllImportStepsFromProfile(config.INSTALL_PROFILE)
+
+
+def install_add_ons(context):
+    """Install additional available add-ons."""
+    if not context.readDataFile('plone.mls.listing_various.txt'):
+        return
+
+    setup = api.portal.get_tool(name='portal_setup')
+    quickinstaller = api.portal.get_tool(name='portal_quickinstaller')
+    for item in ADD_ONS:
+        try:
+            pkg_resources.get_distribution(item)
+        except pkg_resources.DistributionNotFound:
+            continue
+        # Only install add-ons which are not installed yet.
+        if not quickinstaller.isProductInstalled(item):
+            if quickinstaller.isProductInstallable(item):
+                try:
+                    profile = 'profile-{0}:default'.format(item)
+                    setup.runAllImportStepsFromProfile(profile)
+                except AttributeError:
+                    pass
