60c79e3326dffa7aad8073a217adb9178a7a3e3a
Thomas Massmann <thomas@propertyshelf.com>
2017-08-08 11:47:41 +0200

    Rerun GS profiles for Plone 5 when doing upgrade from 4 to 5.
    This ensures that Plone5 specific settings are registered.

--- src/ps/plone/mls/setuphandlers.py
+++ src/ps/plone/mls/setuphandlers.py
@@ -3,11 +3,18 @@
 
 # zope imports
 from Products.CMFPlone.interfaces import INonInstallable
+from Products.GenericSetup.interfaces import IProfileImportedEvent
+from plone import api
+from zope.component import adapter
 from zope.interface import implementer
+
+# local imports
+from ps.plone.mls import config
 
 
 @implementer(INonInstallable)
 class HiddenProfiles(object):
+    """Define hidden GenericSetup profiles."""
 
     def getNonInstallableProfiles(self):
         """Hide uninstall profile from site-creation and quickinstaller."""
@@ -15,3 +22,11 @@ class HiddenProfiles(object):
             'ps.plone.mls:testfixture',
             'ps.plone.mls:uninstall',
         ]
+
+
+@adapter(IProfileImportedEvent)
+def handle_profile_imported_event(event):
+    """Update 'last version for profile' after a full import."""
+    if event.profile_id == 'profile-plone.app.upgrade.v50:to50alpha3':
+        setup = api.portal.get_tool(name='portal_setup')
+        setup.runAllImportStepsFromProfile(config.INSTALL_PROFILE)
