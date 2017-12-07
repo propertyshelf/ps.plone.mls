d9809917492427c9e9b7e9fe189ad34727fb6d8f
Thomas Massmann <thomas@propertyshelf.com>
2017-12-07 17:09:59 -0500

    Adjust tests for smart_truncate.

--- src/ps/plone/mls/tests/test_browser_utilities.py
+++ src/ps/plone/mls/tests/test_browser_utilities.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """Test utilities from ps.plone.mls.browser.utilities."""
 
+from plone import api
 from ps.plone.mls.testing import INTEGRATION_TESTING
 
 
@@ -24,19 +25,51 @@ class TestUtilitiesView(unittest.TestCase):
         """Validate the 'smart_truncate' method."""
         TEXT = (
             u'Lorem ipsum dolor sit amet, consectetur adipiscing elit. '
-            u'Aliquam non lorem vulputate leo tincidunt cursus eget ac '
-            u'mauris. Aliquam at orci in elit gravida faucibus eget vel '
-            u'lectus.'
-        )
-
-        truncated = self.view.smart_truncate(TEXT)
-        self.assertEqual(len(truncated), 156)
-        self.assertEqual(
-            truncated,
-            u'Lorem ipsum dolor sit amet, consectetur adipiscing elit. '
-            u'Aliquam non lorem vulputate leo tincidunt cursus eget ac '
-            u'mauris. Aliquam at orci in elit gravida...'
+            u'Mauris suscipit orci sed sollicitudin convallis. Vestibulum '
+            u'fringilla pretium velit, et elementum sapien. Pellentesque '
+            u'habitant morbi tristique senectus et netus et malesuada fames '
+            u'ac turpis egestas. Morbi neque quam, volutpat sed nulla non, '
+            u'placerat volutpat purus. Etiam vitae nibh eget ipsum tincidunt '
+            u'cursus rhoncus fermentum urna. Aliquam in ultrices magna. '
+            u'Pellentesque habitant morbi tristique senectus et netus et '
+            u'malesuada fames ac turpis egestas. Cras mollis lorem nibh, in '
+            u'pharetra lorem tincidunt eget. Etiam ultrices vehicula tortor '
+            u'ut porta. Quisque commodo ex non dapibus tincidunt.'
         )
 
         truncated = self.view.smart_truncate(None)
         self.assertIsNone(truncated)
+
+        truncated = self.view.smart_truncate(TEXT)
+        self.assertEqual(len(truncated), 654)
+
+        api.portal.set_registry_record(
+            name='plone.mls.listing.interfaces.IMLSUISettings.truncate_texts',
+            value=True,
+        )
+
+        truncated = self.view.smart_truncate(TEXT)
+        self.assertEqual(len(truncated), 319)
+
+        self.assertEqual(
+            truncated,
+            u'Lorem ipsum dolor sit amet, consectetur adipiscing elit. '
+            u'Mauris suscipit orci sed sollicitudin convallis. Vestibulum '
+            u'fringilla pretium velit, et elementum sapien. Pellentesque '
+            u'habitant morbi tristique senectus et netus et malesuada fames '
+            u'ac turpis egestas. Morbi neque quam, volutpat sed nulla non, '
+            u'placerat volutpat...'
+        )
+
+        api.portal.set_registry_record(
+            name='plone.mls.listing.interfaces.IMLSUISettings.truncate_length',
+            value=15,
+        )
+
+        truncated = self.view.smart_truncate(TEXT)
+        self.assertEqual(len(truncated), 14)
+
+        self.assertEqual(
+            truncated,
+            u'Lorem ipsum...'
+        )
