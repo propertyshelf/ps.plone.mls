23616121e8e8a87d5b93dd6849b33f865d8ff40e
Thomas Massmann <thomas@propertyshelf.com>
2017-12-07 12:09:48 -0500

    Remove sphinx extras which are not used anymore.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,7 +37,7 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
-    'sphinxcontrib_robotdoc',
+    # 'sphinxcontrib_robotdoc',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
