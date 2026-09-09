b1b334aa8120a2224bc59176062a4313440d25d1
Zach Cashero <zach@propertyshelf.com>
2026-09-09 17:13:28 -0500

    Back to development: 0.22

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 
 from setuptools import setup, find_packages
 
-version = '0.21'
+version = '0.22.dev0'
 description = "Propertyshelf MLS Plone Embedding."
 long_description = ('\n'.join([
     open('README.rst').read(),
